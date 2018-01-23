6efa6b9b9f8dac710869047d22194c75f2fb17ec
kurage- <consumerofair@gmail.com>
2018-01-23 04:24:28 -0500

    Add sorting on wardrobe pages

--- closet/app/controllers/items_controller.rb
+++ closet/app/controllers/items_controller.rb
@@ -49,19 +49,57 @@ class ItemsController < ApplicationController
 
 	def index
 		@user = User.find(params[:user_id])
-		#@items = Item.order(sort_column + " " + sort_direction)
-    @items = Item.all
+    # get items that belong to user we are viewing
+    items = Item.joins(:user).where('users.id = ?', params[:user_id])
+    sort_order = nil
+
+    # order results, default is ASC
+    # if sorting by price, sort by currency first
+    unless params[:column].blank?
+      if params[:column].include? "price"
+        sort_order = "price_currency, " + params[:column].gsub(/\+/, " ")
+      else
+        sort_order = params[:column].gsub(/\+/, " ")
+      end
+    end
+
+    # filter for items from selected brand (if applicable)
+    unless params[:brand].blank?
+      brand = params[:brand].gsub(/\+/, " ")
+      items = items.where('brand = ?', brand)
+    end
+    # filter for selected item type (if applicable)
+    unless params[:item_type].blank?
+      i_type = params[:item_type].gsub(/\+/, " ")
+      items = items.where('item_type = ?', i_type)
+    end
+    # filter for selected hair accessory type (if applicable)
+    unless params[:ha_type].blank?
+      ha_type = params[:ha_type].gsub(/\+/, " ")
+      items = items.where('ha_type = ?', ha_type)
+    end
+    # order results by desired attribute
+    if sort_order
+      items = items.order(sort_order)
+    end
+    if params[:arrived] == '2'
+      items = items.where('notarrived = ?', false)
+    elsif params[:arrived] == '3'
+      items = items.where('notarrived = ?', true)
+    end
+    @items = items
   end
 
 	private
 		def item_params
 			params.require(:item).permit(:name, :img, :ref_url, :o_name, 
 				:brand, :item_type, :ha_type, :color, :p_price, :f_price, :price_currency, :date_p, :date_r, :notes, :notarrived)
-		end
+    end
+
 		def logged_in_user
 			unless logged_in?
 				flash[:danger] = "Please log in."
 				redirect_to login_url
 			end
-		end
+    end
 end
